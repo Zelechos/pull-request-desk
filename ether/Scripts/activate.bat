06eafffc8e14fca91bbb96805805520ecb50780e
Alex T.H. <zelechos@hotmail.com>
2024-04-26 15:24:00 -0400

    feat(v0.0.0): add new project of AI

---
+++ ether/Scripts/activate.bat
@@ -0,0 +1,43 @@
+@echo off
+
+rem This file is UTF-8 encoded, so we need to update the current code page while executing it
+for /f "tokens=2 delims=:." %%a in ('"%SystemRoot%\System32\chcp.com"') do (
+    set _OLD_CODEPAGE=%%a
+)
+if defined _OLD_CODEPAGE (
+    "%SystemRoot%\System32\chcp.com" 65001 > nul
+)
+
+set VIRTUAL_ENV=L:\Repositorios\Ether\ether\ether
+
+if not defined PROMPT set PROMPT=$P$G
+
+if defined _OLD_VIRTUAL_PROMPT set PROMPT=%_OLD_VIRTUAL_PROMPT%
+if defined _OLD_VIRTUAL_PYTHONHOME set PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%
+
+set _OLD_VIRTUAL_PROMPT=%PROMPT%
+set PROMPT=(ether) %PROMPT%
+
+if defined PYTHONHOME set _OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%
+set PYTHONHOME=
+
+if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
+if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
+
+set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
+set VIRTUAL_ENV_PROMPT=(ether) 
+
+:END
+if defined _OLD_CODEPAGE (
+    "%SystemRoot%\System32\chcp.com" %_OLD_CODEPAGE% > nul
+    set _OLD_CODEPAGE=
+)
+
+@echo off
+
+rem Carga las variables de entorno desde el archivo .env si existe
+if exist "%VIRTUAL_ENV%\Scripts\python.exe" (
+    for /f "usebackq delims=" %%i in ("%VIRTUAL_ENV%\.env") do (
+        set "%%i"
+    )
+)
---
+++ ether/app/ether.py
@@ -0,0 +1,20 @@
+import os
+
+from groq import Groq
+
+client = Groq(
+    api_key=os.environ.get("GROQ_API_KEY"),
+)
+
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": "Creame un codigo CSS para esta etiqueta html directamente para copiar: <h1>",
+        }
+    ],
+    model="llama3-70b-8192",
+)
+
+print(chat_completion.choices[0].message.content)
